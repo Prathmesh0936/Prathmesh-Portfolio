cab84a9b56f5fa825aad1ee16db425f4e5b8cf5d
Your Name <your.email@example.com>
2025-12-14 16:47:01 +0530

    Update footer and favicon files

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            © Developer Portfolio by <Link target="_blank" href="https://linkedin.com/in/prathmesh-kshirsagar-157889236" className="text-[#16f2b3]">Prathmesh Kshirsagar</Link>
+            © Develop by <Link target="_blank" href="https://linkedin.com/in/prathmesh-kshirsagar-157889236" className="text-[#16f2b3]">Prathmesh Kshirsagar</Link>
           </p>
           <div className="flex items-center gap-5">
           </div>
